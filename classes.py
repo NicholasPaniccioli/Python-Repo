8c89250f90920dea386db1a226e8d690bab02f2e
Nicholas Paniccioli <nicholaspanic20@gmail.com>
2025-05-14 12:41:51 -0400

    Classes!
    Began working on classes and understanding their syntax. How they make a constructor using 'init'. Fairly similar to all OOP languages I've used

---
+++ classes.py
@@ -0,0 +1,27 @@
+class Shark:
+    def __init__(self, name, age, hat):
+        self.name = name
+        self.age = age
+        self.hat = hat
+
+    def swim(self):
+        print(self.name + " is swimming.")
+
+    def be_awesome(self):
+        print(self.name + " is being awesome.")
+
+    def wearing_hat(self):
+        print(self.name + " is wearing a " + self.hat)
+
+def main():
+    sammy = Shark("Sammy", 10, "Cowboy")
+    sammy.swim()
+    sammy.wearing_hat()
+    stevie = Shark("Stevie", 5, "Beanie")
+    stevie.be_awesome()
+    stevie.wearing_hat()
+
+    print("Stevie and Sammy have a combined age of: " + str(stevie.age + sammy.age))
+
+if __name__ == "__main__":
+    main()
