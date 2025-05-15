2bf92fbfa7ef584a9bacc48baa6f72066e608c7f
Nicholas Paniccioli <nicholaspanic20@gmail.com>
2025-05-15 16:41:17 -0400

    Added to exercise
    To get a bit more familiar with old habits, added some more functionality and cleaned up the exercise I did a bit.

--- Mosh-Exercises/weight-converter.py
+++ Mosh-Exercises/weight-converter.py
@@ -1,18 +1,28 @@
 # Exercise is to take an input/weight
 # And convert it to either Kilograms or Pounds
+loop_exit = True
 
-#Take and store user initial input
-weight = float(input("Weight: "))
+while loop_exit:
+    #Take and store user initial input
+    weight = float(input("\nWeight: "))
 
-#Ask for either Kg or Lbs
-weight_unit = input("(K)g or (L)bs: ")
+    #Ask for either Kg or Lbs
+    weight_unit = input("(K)g or (L)bs: ").lower()
 
-#Return the converted value
-if weight_unit.lower() == "k":
-    #Kg to Lbs
-    weight_conversion = weight * 2.2
-    print("Weight in Lbs: " + str(weight_conversion))
-elif weight_unit.lower() == "l":
-    #Lbs to Kg
-    weight_conversion = weight / 2.2
-    print("Weight in Kg: " + str(weight_conversion))
+    #Return the converted value
+    if weight_unit == "k":
+        #Kg to Lbs
+        weight_conversion = round(weight * 2.2, 2)
+        print("Weight in Lbs: " + str(weight_conversion))
+    elif weight_unit == "l":
+        #Lbs to Kg
+        weight_conversion = round(weight / 2.2, 2)
+        print("Weight in Kg: " + str(weight_conversion))
+
+    if exit == True:
+        user_quit = input("\nWould you like to (Q)uit: ").lower()
+
+        if user_quit == "q":
+            loop_exit = False
+            print("Goodbye!\n")
+            
